8d8df2c0a99bb7c7fce6cd748c3f74ec3e1b8533
WebF0x <webf0x@hotmail.com>
2013-12-24 21:42:37 -0500

    Organize folders

---
+++ CaseBasedAI/CaseBasedAI.h
@@ -0,0 +1,48 @@
+#ifndef AI_H
+#define AI_H
+
+#include <vector>
+#include "Decision.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define INVALID_REACTION (-1)
+
+class AI
+{
+    public:
+        AI(const std::vector<int>& possibleReactions);
+        virtual ~AI();
+
+        int getReaction(const Situation&);
+        void forceDecision(const Decision&);
+
+        void learn(const Decision&);
+        void forget(const Decision&);
+
+        void reward();
+        void punish();
+
+        bool saveMemory(const std::string&) const ;
+        bool loadMemory(const std::string&);
+
+    private:
+        /**
+            On veut utiliser des pointeurs de décisions au lieu de copier les décisions au complet.
+            Gain d'efficacité et meilleure gestion de la mémoire
+        */
+        std::vector<Decision*> m_memory2;
+
+        std::vector<int> m_possibleReactions;
+        Decision *m_lastDecision;
+
+        void makeDecision(const Decision&);
+        void makeDecision(const Situation&, int, const Result&);
+        void clearDoubles();
+        void memorizeDecision(const Decision&);
+        std::vector<Decision*> getPastDecisions(const Situation& situation, Result resultWanted);
+        int newReaction(const std::vector<Decision*>& possibleDecisions);
+};
+
+#endif // AI_H

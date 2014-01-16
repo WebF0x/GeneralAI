0c61c9a30d8ed7fde919776111eacff4a0995384
WebF0x <webf0x@hotmail.com>
2014-01-16 02:06:00 -0500

    main tests with bigger vectors

--- AIs/GeneralAI/main.cpp
+++ AIs/GeneralAI/main.cpp
@@ -8,12 +8,11 @@ class DummyAI : public GeneralAI
     public:
         DummyAI(int a, int b, int c, int d) : GeneralAI(a,b,c,d) {}
 
-        virtual vector<int> coreOutput(const vector<int>& input) const {return vector<int>(1);}
+        virtual vector<int> coreOutput(const vector<int>& input) const {cout<<"Outputting"<<endl; return vector<int>(OUTPUT_SIZE);}
 
 
     protected:
-        virtual void coreLearn(const vector<int>& input, const vector<int>& output, float outcome) {}
-        //virtual vector<int> coreOutput(const vector<int>& input) const {return vector<int>();}
+        virtual void coreLearn(const vector<int>& input, const vector<int>& output, float outcome) {cout<<"Learning"<<endl;}
 
         virtual vector<int> getMemory() const{return vector<int>();}
         virtual void setMemory(vector<int> memory) {}
@@ -21,11 +20,11 @@ class DummyAI : public GeneralAI
 
 int main()
 {
-    const unsigned int inputSize(1), outputSize(1);
-    const int maxInput(1), maxOutput(1);
+    const unsigned int inputSize(10), outputSize(10);
+    const int maxInput(3), maxOutput(100);
     const string fileName = "myDummySave";
 
-    //Create DummyAI
+    //Create GeneralAI
     GeneralAI *ai = new DummyAI(inputSize,outputSize,maxInput,maxOutput);
 
     //load
@@ -41,8 +40,13 @@ int main()
     //output
     try
     {
-        vector<int> v(1);
-        ai->output(v);
+        vector<int> v(inputSize);
+        vector<int> v2 = ai->output(v);
+
+        for(unsigned int i=0; i<outputSize; ++i)
+        {
+            cout<<v2[i]<<endl;
+        }
     }
     catch(exception& e)
     {
@@ -52,7 +56,7 @@ int main()
     //learn
     try
     {
-        vector<int> input(1), output(1);
+        vector<int> input(inputSize), output(outputSize);
         float outcome = -1;
         ai->learn(input, output, outcome);
     }
@@ -71,7 +75,7 @@ int main()
         cout<<e.what()<<endl;
     }
 
-    //Delete DummyAI
+    //Delete GeneralAI
     delete ai;
 
     return 0;

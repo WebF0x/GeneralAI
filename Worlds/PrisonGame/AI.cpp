b173d9a91646f0d832a496acd6f647984a2e2823
WebF0x <webf0x@hotmail.com>
2014-01-22 16:22:18 -0500

    Game I previously coded, doesn't work with GeneralAI yet

---
+++ Worlds/PrisonGame/AI.cpp
@@ -0,0 +1,246 @@
+#include "AI.h"
+
+using namespace std;
+
+/**/
+#include <cstdlib>
+
+AI::AI(const std::vector<int>& possibleReactions) :
+    m_possibleReactions(possibleReactions), m_lastDecision(NULL)
+{
+}
+
+AI::~AI()
+{
+    for(unsigned int i(0); i<m_memory2.size(); i++)
+    {
+        delete m_memory2[i];
+    }
+    delete m_lastDecision;
+}
+
+void AI::makeDecision(const Decision& decision)
+{
+    delete m_lastDecision;
+    m_lastDecision = new Decision(decision);
+    memorizeDecision(decision);
+}
+
+
+void AI::makeDecision(const Situation& sit, int reaction, const Result& res)
+{
+    makeDecision(Decision(sit,reaction,res));
+}
+
+void AI::memorizeDecision(const Decision& decision)
+{
+    ///Vérifier si on se souvient d'une décision identique
+    bool similarSituationExists = false;
+    for(unsigned int i(0); i<m_memory2.size(); i++)
+    {
+        ///Memoire de décision non-Bad: on la remplace
+        if( (m_memory2[i]->getSituation() == decision.getSituation()) &&
+                (m_memory2[i]->getReaction()  == decision.getReaction())  &&
+                (m_memory2[i]->getResult()    != Bad))
+        {
+            m_memory2.erase(m_memory2.begin() + i);
+            m_memory2.push_back(new Decision(decision));
+        }
+
+        similarSituationExists = similarSituationExists || ((m_memory2[i]->getSituation() == decision.getSituation()) &&
+                                 (m_memory2[i]->getReaction()  == decision.getReaction()));
+    }
+
+    if(!similarSituationExists)
+    {
+        m_memory2.push_back(new Decision(decision));
+    }
+
+    clearDoubles();
+}
+
+void AI::clearDoubles()
+{
+    bool repeat;
+    do
+    {
+        repeat = false;
+        for(unsigned int i(0); i<m_memory2.size(); i++)
+        {
+            for(unsigned int j(i+1); j<m_memory2.size(); j++)
+            {
+                if( *m_memory2[i] == *m_memory2[j] )
+                {
+                    m_memory2.erase(m_memory2.begin() + j);
+                    repeat = true;
+                }
+
+            }
+        }
+    }
+    while (repeat);
+}
+
+void AI::forceDecision(const Decision& decision)
+{
+    makeDecision(decision);
+}
+
+int AI::getReaction(const Situation& currentSituation)
+{
+    ///Mimic good past Decisions
+    std::vector<Decision*> goodPastDecisions     = getPastDecisions(currentSituation , Good);
+    if (!goodPastDecisions.empty())
+    {
+        makeDecision(*goodPastDecisions[0]);
+        return goodPastDecisions[0]->getReaction();
+    }
+
+    std::vector<Decision*> neutralPastDecisions  = getPastDecisions(currentSituation , Neutral);
+    std::vector<Decision*> badPastDecisions      = getPastDecisions(currentSituation , Bad);
+
+    /// Try a new reaction
+    std::vector<Decision*> previousDecisions;
+    previousDecisions.reserve(neutralPastDecisions.size() + badPastDecisions.size());
+    previousDecisions.insert( previousDecisions.end(), neutralPastDecisions.begin(), neutralPastDecisions.end() );
+    previousDecisions.insert( previousDecisions.end(), badPastDecisions.begin(), badPastDecisions.end() );
+
+    int potentialReaction = newReaction(previousDecisions);
+    if (potentialReaction != INVALID_REACTION)
+    {
+        makeDecision(currentSituation, potentialReaction, Neutral);
+        return potentialReaction;
+    }
+
+    ///Try a neutral reaction
+    if(!neutralPastDecisions.empty())
+    {
+        makeDecision(*neutralPastDecisions[0]);
+        return neutralPastDecisions[0]->getReaction();
+    }
+    else
+        ///All possible reactions are Bad
+        ///     ===> The last decision was bad
+    {
+        punish();
+        return INVALID_REACTION;
+    }
+}
+
+void AI::learn(const Decision& decision)
+{
+    memorizeDecision(decision);
+}
+
+void AI::forget(const Decision& decision)
+{
+    for (unsigned int i(0); i<m_memory2.size(); i++)
+    {
+        if(*m_memory2[i] == decision)
+        {
+            m_memory2.erase(m_memory2.begin() + i);
+            cout<<"Memoire effacee"<<endl;
+            break;
+        }
+    }
+}
+
+void AI::reward()
+{
+    if (m_lastDecision != NULL)
+    {
+        m_lastDecision->setResult(Good);
+        memorizeDecision(*m_lastDecision);
+    }
+}
+
+void AI::punish()
+{
+    if (m_lastDecision != NULL)
+    {
+        m_lastDecision->setResult(Bad);
+        memorizeDecision(*m_lastDecision);
+    }
+}
+
+std::vector<Decision*> AI::getPastDecisions(const Situation& situation, Result resultWanted)
+{
+    std::vector<Decision*> decisionVector;
+    for(unsigned int i(0); i<m_memory2.size(); i++)
+    {
+        if( (m_memory2[i]->getSituation() == situation) &&
+                (m_memory2[i]->getResult() == resultWanted))
+        {
+            decisionVector.push_back(m_memory2[i]);
+        }
+    }
+    return decisionVector;
+}
+
+int AI::newReaction(const std::vector<Decision*>& possibleDecisions)
+{
+    ///Try to find a new reaction
+    for (unsigned int i(0); i<m_possibleReactions.size(); i++)
+    {
+        bool isNewReaction = true;
+        for (unsigned int j(0); j<possibleDecisions.size(); j++)
+        {
+            if (possibleDecisions[j]->getReaction() == m_possibleReactions[i])
+            {
+                isNewReaction = false;
+            }
+        }
+
+        if (isNewReaction)
+        {
+            return m_possibleReactions[i];
+        }
+    }
+    return INVALID_REACTION;
+}
+
+bool AI::saveMemory(const string& fileName)
+{
+    ///Open saveFile
+    ofstream saveFile;
+    saveFile.open(fileName.data());
+    if (!saveFile.is_open())
+        return false;
+
+    ///Write memory in file
+    for(unsigned int i(0); i<m_memory2.size(); i++)
+    {
+        saveFile<<m_memory2[i]->toString()<<endl;
+    }
+
+    saveFile.close();
+    return true;
+}
+
+bool AI::loadMemory(const string& fileName)
+{
+    ///Open saveFile
+    ifstream saveFile;
+    saveFile.open(fileName.data());
+    if(!saveFile.is_open())
+    {
+        cout<<"File does not exist!"<<endl;
+        return false;
+    }
+    else
+    {
+        /// Recreate memory from file
+        string saveData;
+        while (getline(saveFile, saveData))
+        {
+            m_memory2.push_back(new Decision(Decision::decisionFromString(saveData)));
+        }
+
+        saveFile.close();
+        cout<<"Memory loaded"<<endl;
+        return true;
+    }
+}
+
+
+
---
+++ Worlds/PrisonGame/Decision.cpp
@@ -0,0 +1,138 @@
+#include "Decision.h"
+
+using namespace std;
+
+Decision::Decision(const Situation& situation, const int reaction, const Result& result):
+    m_situation(situation), m_reaction(reaction), m_result(result)
+{
+
+}
+
+Decision Decision::decisionFromString(string data)
+{
+    ///Will store info on substrings
+    string situationData, reactionData, resultData;
+
+    ///Découper en substrings
+    unsigned int i(0);
+    for(i=0; i<data.length(); i++)
+    {
+        /// Trouver le caractère ';'
+        if(data[i] == ';')
+        {
+            situationData = data.substr(0,i);
+            i++;
+            break;
+        }
+    }
+
+    for(unsigned int j(i); j<data.length(); j++)
+    {
+        /// Trouver le 2e caractère ";"
+        if(data[j] == ';')
+        {
+            reactionData = data.substr(i,j-i);
+            j++;
+            resultData = data.substr(j,data.length()-j-i);
+            break;
+        }
+    }
+
+    ///Générer les éléments d'une décision
+    Situation tempSituation(Situation::fromString(situationData));
+
+    stringstream ss;
+    int tempReaction;
+    ss<<reactionData;
+    ss>>tempReaction;
+
+    Result tempResult (resultFromString(resultData));
+
+    return Decision(tempSituation, tempReaction, tempResult);
+}
+
+Result Decision::resultFromString(const string& str)
+{
+    if(str == "0")
+        return Good;
+
+    else if(str == "2")
+        return Bad;
+
+    else
+        return Neutral;
+}
+
+Situation Decision::getSituation()  const
+{
+    return m_situation;
+}
+void Decision::setSituation(const Situation& s)
+{
+    m_situation = s;
+}
+
+int Decision::getReaction() const
+{
+    return m_reaction;
+}
+void Decision::setReaction(const int r)
+{
+    m_reaction = r;
+}
+
+Result Decision::getResult() const
+{
+    return m_result;
+}
+void Decision::setResult(const Result& r)
+{
+    m_result = r;
+}
+
+Decision Decision::operator=(const Decision& decision)
+{
+    m_situation = decision.m_situation;
+    m_reaction = decision.m_reaction;
+    m_result = decision.m_result;
+
+    return *this;
+}
+
+bool Decision::operator==(const Decision& decision) const
+{
+    return ((m_situation == decision.m_situation) &&
+            (m_reaction == decision.m_reaction) &&
+            (m_result == decision.m_result));
+}
+
+string Decision::toString() const
+{
+    stringstream ss;
+
+    //Situation
+    ss << m_situation.toString();
+    ss << ";";
+
+    //Reaction
+    ss << m_reaction;
+    ss << ";";
+
+    //Result
+    ss << m_result;
+
+
+    return ss.str();
+}
+
+
+
+
+
+
+
+
+
+
+
+

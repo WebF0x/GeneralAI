2252855d5c72811d84dfc2afa64979cc0a60f9c3
WebF0x <webf0x@hotmail.com>
2016-04-12 18:29:49 -0400

    Test NeuralNetAI for save/load

--- test/NeuralNetAITest.cpp
+++ test/NeuralNetAITest.cpp
@@ -13,4 +13,31 @@ SUITE( NeuralNetAITest )
 
         ai.learn( 1 );
     }
+
+    TEST( saveAndLoad )
+    {
+        const std::string saveFileName = "NeuralNetAI.save";
+        const int inputSize       = 1;
+        const int outputSize      = 1;
+        const int inputAmplitude  = 1;
+        const int outputAmplitude = 1;
+        const double tolerance    = 0.01;
+
+        NeuralNetAI originalAI = NeuralNetAI( inputSize, outputSize, inputAmplitude, outputAmplitude );
+        NeuralNetAI cloneAI    = NeuralNetAI( inputSize, outputSize, inputAmplitude, outputAmplitude );
+
+        originalAI.mutate();
+        cloneAI.mutate();
+
+        GeneralAI::save< NeuralNetAI >( originalAI, saveFileName );
+        GeneralAI::load< NeuralNetAI >( cloneAI,    saveFileName );
+
+        const auto cloneOutput    = cloneAI.output();
+        const auto originalOutput = originalAI.output();
+
+        CHECK_ARRAY_CLOSE( originalOutput, cloneOutput, outputSize, tolerance );
+
+        // Delete save file
+        CHECK( !remove( saveFileName.data() ) ) ;
+    }
 }

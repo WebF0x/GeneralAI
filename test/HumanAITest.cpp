e892cf10e194dc62c9ac3dc09919c55faf3bf86a
WebF0x <webf0x@hotmail.com>
2016-05-04 21:27:55 -0400

    Smoketest for HumanAI

---
+++ test/HumanAITest.cpp
@@ -0,0 +1,16 @@
+#include "UnitTest++/UnitTest++.h"
+
+#include "StrongAI/AI/HumanAI/HumanAI.hpp"
+
+SUITE( HumanAITest )
+{
+    TEST( smokeTest )
+    {
+        HumanAI ai( 0, 0, 0, 0 );
+
+        auto output = ai.output();
+        CHECK_EQUAL( 0, output.size() );
+
+        ai.learn( 1 );
+    }
+}

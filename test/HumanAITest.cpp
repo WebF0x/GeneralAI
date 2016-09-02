03e3b8139b7ddc7e795fe3ddc8baa8bd0d349227
WebF0x <webf0x@hotmail.com>
2016-09-02 08:47:19 -0400

    Remove output from HumanAITest for sanity reasons

--- test/HumanAITest.cpp
+++ test/HumanAITest.cpp
@@ -7,10 +7,5 @@ SUITE( HumanAITest )
     TEST( smokeTest )
     {
         HumanAI ai( 0, 0, 0, 0 );
-
-        auto output = ai.output();
-        CHECK_EQUAL( 0, output.size() );
-
-        ai.learn( 1 );
     }
 }
